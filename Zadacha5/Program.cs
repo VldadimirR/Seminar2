4fe91647e895edbdfd634e6bd68dc2e4c6a16e8d
VladimirR <Vova.ru0007@yandex.ru>
2022-08-13 16:26:06 +0500

    5

---
+++ Zadacha5/Program.cs
@@ -0,0 +1,26 @@
+﻿void CheckMultiple(int number1, int number2)
+{
+    if (number2 == number1 * number1)
+    {
+        Console.WriteLine("Да, является квадратом");
+    }
+    else
+    {
+        Console.WriteLine("Нет, не является квадратом");
+    }
+
+}
+Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 < number2)
+{
+    CheckMultiple(number1, number2);
+}
+else
+{
+    CheckMultiple(number2, number1);
+}
